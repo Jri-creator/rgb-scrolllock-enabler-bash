1c95524b76bdb4e396ac4be48429ed65a90ccbee
Jacob Butcher <145695642+Jri-creator@users.noreply.github.com>
2025-01-22 23:32:44 -0500

    Update rgb_new.sh

--- rgb_new.sh
+++ rgb_new.sh
@@ -2,7 +2,12 @@
 
 # Function to display menu and execute commands
 function toggle_rgb {
-    if [[ "$1" == "-on" ]]; then
+    if [[ "$1" == "-reset" ]]; then
+        echo "Resetting RGB..."
+        sudo bash -c "/usr/bin/setleds -L -scroll < /dev/console"
+        sudo bash -c "/usr/bin/setleds -L +scroll < /dev/console"
+        exit 0
+    elif [[ "$1" == "-on" ]]; then
         echo "Turning RGB On..."
         sudo bash -c "/usr/bin/setleds -L +scroll < /dev/console"
         exit 0
@@ -13,10 +18,12 @@ function toggle_rgb {
     fi
 
     echo "Did you know that you could use -on or -off to control RGB without this menu?"
+    echo "If your RGB turned off when pressing Caps or Num Lock, use -reset !"
     echo "Select an option:"
     echo "1. Turn RGB On"
     echo "2. Turn RGB Off"
-    read -p "Enter your choice (1 or 2): " choice
+    echo "3. Reset RGB"
+    read -p "Enter your choice (1, 2, 3): " choice
 
     case $choice in
         1)
@@ -25,6 +32,11 @@ function toggle_rgb {
             ;;
         2)
             echo "Turning RGB Off..."
+            sudo bash -c "/usr/bin/setleds -L -scroll < /dev/console"
+            ;;
+        3)
+            echo "Resetting RGB..."
+            sudo bash -c "/usr/bin/setleds -L +scroll < /dev/console"
             sudo bash -c "/usr/bin/setleds -L -scroll < /dev/console"
             ;;
         *)
